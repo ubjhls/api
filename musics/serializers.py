14fcf596ff5d480e235c1628c431d4d2152b946b
kimseonghoon <ubjhls@naver.com>
2019-10-29 11:51:53 +0900

    Artist, Review

--- musics/serializers.py
+++ musics/serializers.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
-from .models import Music
+from .models import Music, Artist, Review
 
 class MusicSerializers(serializers.ModelSerializer):
     class Meta:
         model = Music
-        fields = ('id', 'title', 'artist_id')
+        fields = ['id', 'title', 'artist_id']
+
+class ArtistSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Artist
+        fields = ['id', 'name']
+
+class ArtistDetailSerializers(serializers.ModelSerializer):
+    music_set = MusicSerializers(many=True)
+    class Meta(ArtistSerializers.Meta):
+        fields = ArtistSerializers.Meta.fields + ['music_set']
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ['content']
--- musics/views.py
+++ musics/views.py
@@ -1,18 +1,64 @@
 from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Music
-from .serializers import MusicSerializers
+from .models import Music, Artist, Review
+from .serializers import MusicSerializers, ArtistSerializers,ArtistDetailSerializers,ReviewSerializer
 # Create your views here.
 
 @api_view(['GET'])  # HTTP method
 def index(request):
+    '''
+    음악 목록 정보
+    '''
     musics = Music.objects.all()
     serializer = MusicSerializers(musics, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
 def detail(request, music_pk):
+    '''
+    음악 상세 정보
+    '''
     music = get_object_or_404(Music, pk=music_pk)
     serializer = MusicSerializers(music)
-    return Response(serializer.date)
+    return Response(serializer.data)
+
+# 아티스트 목록
+@api_view(['GET'])
+def artists_index(request):
+    '''
+    아티스트 목록
+    '''
+    artists = Artist.objects.all()
+    serializer = ArtistSerializers(artists, many=True)
+    return Response(serializer.data)
+
+# 아티스트 상세보기
+@api_view(['GET'])
+def artist_detail(request, artist_pk):
+    '''
+    아티스트와 해당하는 음악
+    '''
+    artist = get_object_or_404(Artist, pk=artist_pk)
+    serializer = ArtistDetailSerializers(artist)
+    return Response(serializer.data)
+
+# 리뷰 작성
+@api_view(['POST'])
+def review_create(request, music_pk):
+    serializer = ReviewSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save(music_id=music_pk)
+    return Response({'message': 'review가 등록 되었습니다   '})
+
+@api_view(['PUT','DELETE'])
+def review_update_delete(request, review_pk):
+    review = get_object_or_404(Review, pk=review_pk)
+    if request.method == 'PUT':
+        serializer = ReviewSerializer(data=request.data, instance=review)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response({'message' : '성공적으로 수정 되었습니다.'})
+    else:
+        review.delete()
+        return Response({'message' : '성공적으로 삭제 되었습니다.'})
